c7a4e5fefd46dcf0ab97894df67213020b0d6558
Martin <mi8301@gmail.com>
2015-11-11 22:00:02 +0100

    Added a testproject with test that should fail but does not do that...

---
+++ FlightBooker.Test/FlightBookerSagasTests.cs
@@ -0,0 +1,22 @@
+﻿using FlightBooker.Host;
+using FlightBooker.Messages;
+using NUnit.Framework;
+
+namespace FlightBooker.Test
+{
+     [TestFixture]
+    public class FlightBookerSagaTests
+    {
+        [Test]
+        public void WhenFlightBookingTimesOut()
+        {
+            var customerId = 1;
+            var startBookingCommand = new StartBookingCommand { CustomerId = customerId, TotalTicketAmount = 11000 };
+
+            NServiceBus.Testing.Test.Saga<FlightBookerSaga>()
+                .When(s => s.Handle(startBookingCommand))
+                .WhenSagaTimesOut()
+                .ExpectPublish<UpgradeCustomerToVIP>(d => d.CustomerId == customerId);
+        }
+    }
+}
